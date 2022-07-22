8d8cd047d4c52007a02743c15c41cdd97f03eb4f
kennethachan <kennethandrechan@gmail.com>
2022-07-22 15:57:22 -0400

    backend and frontend created

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg"
+import "./App.css"
+import GIFs from "./components/GIFs"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <GIFs />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/GIFs.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function GIFs(props) {
+  return <div></div>
+}
+
+export default GIFs
