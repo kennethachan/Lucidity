f07ff1b28a162582c070124b0f59a470e78fc1a1
kennethachan <kennethandrechan@gmail.com>
2022-07-22 22:46:27 -0400

    app.get connected

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const NoteSchema = new Schema(
+const Note = new Schema(
   {
     text: { type: String, required: true },
     complete: { type: String, default: false },
   },
   { timestamps: true }
 )
-
-module.exports = NoteSchema
+module.exports = mongoose.model("Note", Note)
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const cors = require("cors")
 const logger = require("morgan")
+const db = require("./db")
+const Note = require("./models/Notes")
+
 const PORT = process.env.PORT || 3001
 
 const app = express()
@@ -11,6 +14,19 @@ app.use(logger("dev"))
 
 app.get("/", (req, res) => {
   res.send("This is root!")
+})
+
+app.get("/notes", async (req, res) => {
+  const notes = await Note.find()
+  res.send(notes)
+})
+
+app.post("/notes/new", (req, res) => {
+  const newNote = new Note({
+    text: req.body.text,
+  })
+  newNote.save()
+  res.json(newNote)
 })
 
 app.listen(PORT, () => {
