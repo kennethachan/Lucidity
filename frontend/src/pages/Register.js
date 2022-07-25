b1223a6a3ee70c5c59ef62d57fb1d7318a39f0e5
kennethachan <kennethandrechan@gmail.com>
2022-07-24 22:42:33 -0400

    functional register button

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom"
 import { useState, useEffect, createRef, useContext } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
-import UserContext from "../components/UserContext"
 
 function Register(props) {
   const image = require("../gifs/shoe.gif")
   const URL = "http://localhost:3001"
   const navigate = useNavigate()
-  const ref = createRef()
+
+  //createContext is code sourced online
+  const UserContext = React.createContext({})
+  const user = useContext(UserContext)
+
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const user = useContext(UserContext)
+  const [changePage, setchangePage] = useState(false)
 
   const register = async (e) => {
     e.preventDefault()
@@ -22,7 +25,12 @@ function Register(props) {
         user.setEmail(response.cred.email)
         setEmail("")
         setPassword("")
+        setchangePage(true)
       })
+  }
+
+  if (changePage == true) {
+    return
   }
 
   return (
@@ -63,6 +71,11 @@ function Register(props) {
           <br />
           <button type="submit" className="register-btn">
             Register
+          </button>
+          <button className="back-btn">
+            <Link className="back-btn" to="/landing">
+              Back
+            </Link>
           </button>
         </form>
       </div>
